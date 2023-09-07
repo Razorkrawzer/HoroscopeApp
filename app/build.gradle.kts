774a444d11a1e116a567cb33c45e2fc65caa21f0
George C <jorge.crco4@gmail.com>
2023-09-07 01:26:47 -0600

    hilt initializer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -10,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.george.horoscapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -36,6 +38,9 @@ android {
     buildFeatures{
         viewBinding = true
     }
+    kotlin{
+        jvmToolchain(8)
+    }
 }
 
 dependencies {
@@ -45,6 +50,10 @@ dependencies {
     //NavComponent
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //DaggerHilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/george/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/george/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -7,8 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.george.horoscapp.R
 import com.george.horoscapp.databinding.FragmentHoroscopeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
 
     private var _binding: FragmentHoroscopeBinding? = null
