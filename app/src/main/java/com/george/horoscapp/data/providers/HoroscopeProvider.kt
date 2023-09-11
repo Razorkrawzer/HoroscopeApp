bfc68738a5ad5852cf2a934dc7aea508c52c3690
Jcrespo <jcrespo@likenuuk.com>
2023-09-10 20:03:37 -0600

    added horoscope images, recyclerview, rotation when clicked icons

---
+++ app/src/main/java/com/george/horoscapp/data/providers/HoroscopeProvider.kt
@@ -0,0 +1,24 @@
+package com.george.horoscapp.data.providers
+
+import com.george.horoscapp.domain.model.HoroscopeInfo
+import com.george.horoscapp.domain.model.HoroscopeInfo.*
+import javax.inject.Inject
+
+class HoroscopeProvider @Inject constructor() {
+    fun getHoroscopes(): List<HoroscopeInfo> {
+        return listOf(
+            Aries,
+            Taurus,
+            Gemini,
+            Cancer,
+            Leo,
+            Virgo,
+            Libra,
+            Scorpio,
+            Sagittarius,
+            Capricorn,
+            Aquarius,
+            Pisces,
+        )
+    }
+}
--- app/src/main/java/com/george/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/george/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,26 +1,72 @@
 package com.george.horoscapp.ui.horoscope
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.george.horoscapp.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.george.horoscapp.databinding.FragmentHoroscopeBinding
+import com.george.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
+
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+    private lateinit var horoscopeAdapter: HoroscopeAdapter
 
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initList()
+        initUIState()
+    }
+
+    private fun initList() {
+        horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
+            Toast.makeText(
+                context,
+                getString(it.name),
+                Toast.LENGTH_SHORT
+            ).show()
+        })
+
+        binding.rvHoroscope.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = horoscopeAdapter
+        }
+
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
+                    horoscopeAdapter.updateList(it)
+                }
+            }
+        }
     }
 
 
