19da9e9c8039015cfa966826f72aaa91b032f335
George C <jorge.crco4@gmail.com>
2023-09-13 01:19:16 -0600

    added detail activity preparation

--- app/src/main/java/com/george/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/george/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,22 +3,57 @@ package com.george.horoscapp.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.george.horoscapp.databinding.ActivityHoroscopeDetailBinding
-import com.george.horoscapp.ui.horoscope.HoroscopeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHoroscopeDetailBinding
-    private val horoscopeDetailViewMode: HoroscopeViewModel by viewModels()
+    private val horoscopeDetailViewMode: HoroscopeDetailViewModel by viewModels()
 
     private val args: HoroscopeDetailActivityArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+
         args.type
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewMode.state.collect {
+                    when (it) {
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState() {
+
+    }
+
+    private fun errorState() {
+
+    }
+
+    private fun successState() {
+
     }
 }
